0a7c0454af4410ef0b70ac9561922b158c010c89
Vishesh Gupta <vishesh@stanford.edu>
2015-07-29 18:28:41 -0700

    switched to 4 indent

--- server.py
+++ server.py
@@ -13,111 +13,111 @@ app = Bottle()
 # ------------------------------ UTILITIES --------------------------------
 
 def typename(o):
-  """Returns classname of `o` in single quotes. eg, typename([]) -> 'list'"""
-  return str(type(o))[7:-1]
+    """Returns classname of `o` in single quotes. eg, typename([]) -> 'list'"""
+    return str(type(o))[7:-1]
 
 def reqinfo():
-  """Gives path + method + JSON body of request
+    """Gives path + method + JSON body of request
 
-  This is the minimum information to figure out what went wrong with the 
-  request that was made to the server. 
-  """
-  return {'path':request.path, 
-          'method' : request.method,
-          'body': json.dumps({k:request.params[k] for k in request.params}) 
-                  if request.json == None else json.dumps(request.json)}
+    This is the minimum information to figure out what went wrong with the 
+    request that was made to the server. 
+    """
+    return {'path':request.path, 
+            'method' : request.method,
+            'body': json.dumps({k:request.params[k] for k in request.params}) 
+                    if request.json == None else json.dumps(request.json)}
 
 def jsonabort(status,message):
-  """Usage: jsonabort(400, 'message') - works just like abort, but json.
-  
-  For JSON REST API, need to be able to send consistent error message with 
-  a cause statement and a status code, along with some information about the 
-  request that was made to cause the error. 
-  
-  this is a parallel function to abort (provided by bottle.py) so use 
-  accordingly. however, it raises HTTPResponse, and so will NOT be 
-  caught by other handlers - it's just a clean exit.
-  """
-  if type(status) is not int: 
-    abort(500, 'jsonabort requires integer status, got {}'.format(
-            typename(status)))
-  if type(message) is not str:
-    abort(500, 'jsonabort requires string message, got {}'.format(
-            typename(message)))
+    """Usage: jsonabort(400, 'message') - works just like abort, but json.
+    
+    For JSON REST API, need to be able to send consistent error message with 
+    a cause statement and a status code, along with some information about the 
+    request that was made to cause the error. 
+    
+    this is a parallel function to abort (provided by bottle.py) so use 
+    accordingly. however, it raises HTTPResponse, and so will NOT be 
+    caught by other handlers - it's just a clean exit.
+    """
+    if type(status) is not int: 
+      abort(500, 'jsonabort requires integer status, got {}'.format(
+              typename(status)))
+    if type(message) is not str:
+      abort(500, 'jsonabort requires string message, got {}'.format(
+              typename(message)))
 
-  raise HTTPResponse(status=status, headers={'Content-Type':'application/json'},
-                     # TODO(vishesh): should return the entire request object?
-                     body=json.dumps({'message':message, 'request':reqinfo()}))
+    raise HTTPResponse(status=status, headers={'Content-Type':'application/json'},
+                       # TODO(vishesh): should return the entire request object?
+                       body=json.dumps({'message':message, 'request':reqinfo()}))
 
 def params(keys=[], opts={}, strict=True):
-  """Decorator: Basic request verification for json REST endpoints 
-  
-  Checks that the request is valid json, that the json returned is an 
-  object (since lists can lead to XSS attacks and are discouraged in 
-  json apis) and that all the keys in `keys` are present in the request.
-  
-  In strict mode, the request is not allowed to have extraneous keys 
-  that aren't present in `keys` or `opts`.
-  
-  Most requests will want to be strict, I can't imagine why you would 
-  want a non-key (required field), non-opt (optional field) parameter
-  in the request you're making. What other kinds of fields are there?
-  However, leaving the option here for the time being - it might be useful
-  to relax the requirement while developing and the keys aren't finalized. 
-  """
-  def reqjson(req_fun):
-    # pass through all args to req_fun 
-    def requirejson_wrapper(*args, **kwargs):
+    """Decorator: Basic request verification for json REST endpoints 
+    
+    Checks that the request is valid json, that the json returned is an 
+    object (since lists can lead to XSS attacks and are discouraged in 
+    json apis) and that all the keys in `keys` are present in the request.
+    
+    In strict mode, the request is not allowed to have extraneous keys 
+    that aren't present in `keys` or `opts`.
+    
+    Most requests will want to be strict, I can't imagine why you would 
+    want a non-key (required field), non-opt (optional field) parameter
+    in the request you're making. What other kinds of fields are there?
+    However, leaving the option here for the time being - it might be useful
+    to relax the requirement while developing and the keys aren't finalized. 
+    """
+    def reqjson(req_fun):
+        # pass through all args to req_fun 
+        def requirejson_wrapper(*args, **kwargs):
 
-      # TODO(vishesh): malformed JSON gives 500 error, should give 400,
-      # can't seem to catch the ValueError from json.loads
-      try:
-        # GET/DELETE have no body. PUT/PATCH/POST have bodies.
-        r = None
-        if request.method in ['GET', 'DELETE']:
-          r = {k: request.params[k] for k in request.params}
-        else: 
-          r = request.json
-      except ValueError as e:
-        jsonabort(400, ('Request should be parseable json, got error: '
-                        ''+str(e.args)))
+            # TODO(vishesh): malformed JSON gives 500 error, should give 400,
+            # can't seem to catch the ValueError from json.loads
+            try:
+                # GET/DELETE have no body. PUT/PATCH/POST have bodies.
+                r = None
+                if request.method in ['GET', 'DELETE']:
+                    r = {k: request.params[k] for k in request.params}
+                else: 
+                    r = request.json
+            except ValueError as e:
+                jsonabort(400, ('Request should be parseable json, got error: '
+                              ''+str(e.args)))
 
-      if r == None:
-        # the only time that r will be None is if the json part fails. 
-        # request.params being empty will give an empty dictionary instead,
-        # so this logic is okay (don't need to change the expected 
-        # content-type based on the request method).
-        jsonabort(400, ('Content-Type should be application/json, got '
-                        ''+str(request.content_type)))
+            if r == None:
+                # the only time that r will be None is if the json part fails. 
+                # request.params being empty will give an empty dictionary instead,
+                # so this logic is okay (don't need to change the expected 
+                # content-type based on the request method).
+                jsonabort(400, ('Content-Type should be application/json, got '
+                                ''+str(request.content_type)))
 
-      if type(r) is not dict:
-        jsonabort(400, 'Request must be a JSON object, not {}'.format(
-                  typename(r)))
-      
-      if not all(k in r for k in keys):
-        jsonabort(400, 'Request is missing keys: ' + 
-                  str(list(set(keys) - r.keys())))
+            if type(r) is not dict:
+                jsonabort(400, 'Request must be a JSON object, not {}'.format(
+                          typename(r)))
+            
+            if not all(k in r for k in keys):
+                jsonabort(400, 'Request is missing keys: ' + 
+                          str(list(set(keys) - r.keys())))
 
-      if strict and not all(p in keys or p in opts for p in r):
-        # since we know that all k in keys is present in r
-        # if the lengths are unequal then for sure there are extra keys. 
-        jsonabort(400, 'Strict mode: request has unrecognized keys: ' + 
-                  str(list(r.keys() - set(keys))))
+            if strict and not all(p in keys or p in opts for p in r):
+                # since we know that all k in keys is present in r
+                # if the lengths are unequal then for sure there are extra keys. 
+                jsonabort(400, 'Strict mode: request has unrecognized keys: ' + 
+                          str(list(r.keys() - set(keys))))
 
-      # instead of modifying/using global state, choosing to pass in
-      # the updated request as a param means that the handler functions
-      # are all pure functions of their input params.
-      # 
-      # This should make testing them easier - it's one less thing to mock.       
-      return req_fun(t.merge(opts, r), *args, **kwargs)  
+            # instead of modifying/using global state, choosing to pass in
+            # the updated request as a param means that the handler functions
+            # are all pure functions of their input params.
+            # 
+            # This should make testing them easier - it's one less thing to mock.       
+            return req_fun(t.merge(opts, r), *args, **kwargs)  
 
-    return requirejson_wrapper
-  return reqjson
+        return requirejson_wrapper
+    return reqjson
 
 # converts a dictionary to flat list of key/value pairs. 
 # each key can have multiple values and they will all be unpacked accordingly.
 multipairs=lambda d: list(t.concat(t.map(
-                         lambda i: (lambda k,v: t.concat((k,e) for e in v) 
+                          lambda i: (lambda k,v: t.concat((k,e) for e in v) 
                                                 if isinstance(v,list) 
                                                 else (k,v))(i[0],i[1]), 
                       d.items())))
@@ -126,33 +126,33 @@ multipairs=lambda d: list(t.concat(t.map(
 
 @app.get('/')
 def index():
-  return static_file('index.html', root="static/")
+    return static_file('index.html', root="static/")
 
 # --------------------------- BASIC STATIC ROUTES
 
 @app.get('/img/<resource:path>')
 def staticimages(resource):
-  return static_file(resource, root='static/img/')
+    return static_file(resource, root='static/img/')
 
 @app.get('/fonts/<resource:path>')
 def staticfonts(resource):
-  return static_file(resource, root='static/fonts/')
+    return static_file(resource, root='static/fonts/')
 
 @app.get('/lib/<resource:path>')
 def staticlib(resource):
-  return static_file(resource, root='static/lib/')
+    return static_file(resource, root='static/lib/')
 
 @app.get('/js/<resource:path>')
 def staticjs(resource):
-  return static_file(resource, root='static/js/')
+    return static_file(resource, root='static/js/')
 
 @app.get('/css/<resource:path>')
 def staticcss(resource):
-  return static_file(resource, root='static/css/')
+    return static_file(resource, root='static/css/')
 
 @app.get('/templates/<resource:path>')
 def statictemplates(resource):
-  return static_file(resource, root='static/templates/')
+    return static_file(resource, root='static/templates/')
 
 
 app.run(host='localhost', port=8080, debug=True, reloader=True)
